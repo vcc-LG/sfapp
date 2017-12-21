3a39ee0c2a816b78498fd2df7b9f5a3c93b8ca4c
Leo Garcia <leo@signum-health.com>
2017-12-21 15:53:25 +0000

    started router work

---
+++ app/src/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+    <p>Mad content</p>
+  </div>
+)
+
+export default About;
--- app/src/App.js
+++ app/src/App.js
@@ -1,22 +1,44 @@
-import React, { Component } from 'react';
+import React, {
+    Component
+} from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from 'react-router-dom'
+
 import logo from './logo.gif';
 import './App.css';
 import Users from './Users.js';
 import NewUserForm from './newUserForm.jsx'
+import About from './About.jsx'
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
+    render() {
+        return (
+            <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">League app</h1>
         </header>
+
         <Users></Users>
         <hr/>
         <NewUserForm></NewUserForm>
       </div>
-    );
-  }
+
+        );
+    }
 }
 
 export default App;
+
+//       <Router>
+//   <div>
+//     <ul>
+//       <li><Link to="/about">About</Link></li>
+//     </ul>
+//     <hr/>
+//     <Route path="/about" component={About}/>
+//   </div>
+// </Router>
